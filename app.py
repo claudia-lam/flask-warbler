a1ff613b098e229e67c8f12d6e7e2574a866611d
Claudia Lam <claudiaslam@gmail.com>
2023-07-20 11:20:29 -0700

    add csrf form to g

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def add_user_to_g():
 
     if CURR_USER_KEY in session:
         g.user = User.query.get(session[CURR_USER_KEY])
-
+        g.csrf_form = CSRFProtectForm()
     else:
         g.user = None
 
