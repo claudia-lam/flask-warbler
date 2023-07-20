79ec15c8de12f6f8ffe0dad4c6b08c2239b5bf93
Claudia Lam <claudiaslam@gmail.com>
2023-07-20 15:46:35 -0700

    add skeleton to test profile

--- test_user_views.py
+++ test_user_views.py
@@ -122,3 +122,13 @@ class UserViewTestCase(UserBaseViewTestCase):
             html = resp.text
 
             self.assertIn("test_bio", html)
+
+    def test_profile(self):
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.u1_id
+
+            resp = c.get('/users/profile')
+            html - resp.text
+
+            self.assertEqual(resp.status_code, 200)
